37665ce6d579fe7ddddcc054b6f1864da525aaec
Amr <amr.ibrahim_95@outlook.com>
2025-04-13 21:09:17 +0200

    -Removed a comment

--- write_statistics_overview_to_json.py
+++ write_statistics_overview_to_json.py
@@ -6,6 +6,3 @@ def write_statistics_overview_to_json(statistics_overview):
             json.dump(statistics_overview, f, indent= 4)
     except Exception as e:
         print(f"[Error] An error happened while trying to write the statistics overview to json : {e}")
-
-
-        #kotlin-course-eligibility-task private
